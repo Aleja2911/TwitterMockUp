692983789ab162fd8359dd327d6d2cd6a2c37e65
Your Name <y.geb@web.de>
2020-10-16 13:28:54 +0200

    add pics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
+import TweetCards from './TweetCards.js';
+
 import './App.css';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
   return (
    <div> 
    
-  
+  <TweetCards/>
 
    </div>
   );
