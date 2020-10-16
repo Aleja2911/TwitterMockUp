40d7d1119f47b6a4cf791c7ceb5418b3890e7ab9
Your Name <y.geb@web.de>
2020-10-16 14:20:09 +0200

    start tweet card

--- src/TweetCards.js
+++ src/TweetCards.js
@@ -1,5 +1,8 @@
 import React from "react";
 import './TweetCards.css';
+import giraffe from './giraffe.png';
+import elephant from './elephant.png';
+import monkey from './monkey.png';
 
 
 
@@ -10,63 +13,70 @@ const TweetCards = ()=>{
                     username: 'elli',
                     email: 'elli@whatever.com',
                     password: 'somePassword1',
-                    profilePic: "./elephant.png"
+                    profile_pic: elephant
                     },
                     {
                     id: 2,
                     username: 'moni',
                     email: 'moni@whatever.com',
                     password: 'somePassword2',
-                    profilePic: './monkey.png'
+                    profile_pic: monkey
                     },
                     {
                         id: 3,
                         username: 'gigi',
                         email: 'gigi@whatever.com',
                         password: 'somePassword3',
-                        profilePic: './giraffe.png'
+                        profile_pic: giraffe
                     }],
             tweets: [{
                     id: 1,
-                    tweetText: 'ma boyyyyyy, thanks for being such a wonderful friend',
-                    tweetImage: './tweet1.png',
-                    timeStamp: '01/01/2020',
+                    tweet_text: 'ma boyyyyyy, thanks for being such a wonderful friend',
+                    tweet_image: './tweet1.png',
+                    timestamp: '01/01/2020',
                     likes: 1,
-                    user_id: 1,
-                    profilePic: './elephant.png'
+                    user_id: 1
                     },
                     {
                     id: 1,
-                    tweetText: 'random tweeeeeet',
-                    tweetImage: './tweet2.png',
-                    timeStamp: '01/01/2020',
+                    tweet_text: 'random tweeeeeet',
+                    tweet_image: './tweet2.png',
+                    timestamp: '01/01/2020',
                     likes: 1,
-                    user_id: 2,
-                    profilePic: './monkey.png'
+                    user_id: 2
 
                     },
                     {
                     id: 1,
-                    tweetText: 'random tweeeeeet',
-                    tweetImage: null,
-                    timeStamp: '01/01/2020',
+                    tweet_text: 'random tweeeeeet',
+                    tweet_image: null,
+                    timestamp: '01/01/2020',
                     likes: 1,
-                    user_id: 3,
-                    profilePic: './giraffe.png'
+                    user_id: 3
                     }]
     };
 
-
+//{id, tweet_text, tweet_image, timestamp, likes, userid}
     return(
-        <div >
-        <img alt="" src="./elephant.png"/> 
+        <div className="tweets-container" >
+        <img alt=" hi" src={giraffe} /> 
             {data&&data.tweets.map((tweet,index)=>{
                 return(
                     <>
                     <div key ={tweet.id}style={{border: 'solid 2px black'}} >
-                        <img alt="" src="./elephant.png"/> 
+                        <img alt="hi" src={data.users[index].profile_pic}/> 
                     </div>
-                    </>
+                    <div>
+                        {data.users[index].username}
+                    </div>
+                    <div>
+                        {tweet.timestamp}
+                    </div>
+                    {tweet.tweet_image&& 
+                    <div>
+                    <img src={tweet.tweet_image} alt="tweet"/>
+                    </div>}
+                </>
                 )
             })}
         </div>
